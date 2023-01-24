5e62c8c6269af36ae3f491fdc446fe0cb4bc6495
Joseph <jwillgerona@gmail.com>
2023-01-24 15:57:37 -0800

    added highscore screens, clear/restart buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,48 @@ var op4El = document.getElementById("op4");
 var submitEl = document.getElementById("submit");
 var promtNum = 0;
 var score = 0;
-
+var timeLeft = 60;
 var highScores = [];
+var isOver = false;
+var tArea = document.getElementById("textarea");
+var sText = document.getElementById("subtext");
+var viewEl = document.getElementById("viewHS");
+var clearEl = document.getElementById("clear");
 
-//NEED TO MAKE HIGHSCORES
+
+function init() {
+    var storedHS = JSON.parse(localStorage.getItem("highS"));
+
+    if(storedHS !== null) {
+        highScores = storedHS;
+    }
+
+}
+
+function storeHighS() {
+    localStorage.setItem("highS", JSON.stringify(highScores));
+}
+
 
 
 var qList = ["Commonly used data types DO NOT include: ", "The condition in an if / else statement is enclosed with ______.", "Arrays in JavaScript can be used to store ______.", "String values must be enclosed within ______ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is: "];
 var opList = [["1. strings", "2. booleans", "3. alerts", "4. numbers", 2], ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets", 2], ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above", 3], ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis", 2], ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log", 3]];
-function timer() {
-    var timeLeft = 60;
-    var timeInterval = setInterval(function () {
 
+
+
+function timer() {
+    var timeInterval = setInterval(function () {
+        if (isOver){
+            score = score * timeLeft;// temp
+            headTimer.textContent = "";
+            clearInterval(timeInterval);
+            displayLast();
+        }
         if (timeLeft > 1) {
             headTimer.textContent = ("Timer: " + timeLeft);
             timeLeft--;
         } else {
-            // change screen to score and option to highscore
+            // change screen to highscore and option to highscore
             headTimer.textContent = "";
             clearInterval(timeInterval);
             displayLast();
@@ -38,7 +63,7 @@ function displayNext() {
 
     if (promtNum===0){
         startEl.style.display= "none";
-        document.getElementById("subtext").style.display = "none";
+        sText.style.display = "none";
         op1El.style.display = "block";
         op2El.style.display = "block";
         op3El.style.display = "block";
@@ -53,11 +78,11 @@ function displayNext() {
 
 function displayLast() {
     questionEl.textContent = "All done!";
-    document.getElementById("subtext").style.display = "block";
-    document.getElementById("textarea").style.display = "block";
-    document.getElementById("subtext").textContent="Your score: " + score;
+    sText.style.display = "block";
+    tArea.style.display = "block";
+    sText.textContent="Your score: " + score;
     submitEl.style.display = "block";
-    document.getElementById("textarea").setAttribute('placeholder', "Enter Initials");
+    tArea.setAttribute('placeholder', "Enter Initials");
     op1El.style.display = "none";
     op2El.style.display = "none";
     op3El.style.display = "none";
@@ -66,82 +91,89 @@ function displayLast() {
 
 }
 
+function displayHighScores() {
+    displayLast();
+    questionEl.textContent = "High scores";
+    
+    document.getElementById("restart").style.display = "block";
+    clearEl.style.display = "block";
+
+    for (var i = 0; i < highScores.length; i++) {
+        var hs = highScores[i];
+    
+        var li = document.createElement("li");
+        li.textContent = hs;
+        li.setAttribute("data-index", i);
+
+        sText.appendChild(li);
+        
+    }
+    submitEl.style.display = "none";
+    tArea.style.display = "none";
+    sText.style.listStyle = "none";
+}
+
+
+function opClick(opNum) {
+    document.getElementById("foot").style.display = "block";
+    if(opList[promtNum][4] === opNum) {
+        score++;
+        document.getElementById("foot").textContent = "Correct!";
+    } else {
+        document.getElementById("foot").textContent = "Wrong";
+    }
+    promtNum++;
+    if (promtNum === qList.length){
+        isOver = true;    //timer controls the end of the quiz
+    } else {
+        displayNext();
+    }
+}
+
+viewEl.addEventListener("click", function() {
+    sText.textContent = "";
+    startEl.style.display = "none";
+    displayHighScores();
+});
+
 startEl.addEventListener("click", function () {
     displayNext();
     timer();
 
 });
 
-//HOW TO GRAB TEXT FROM BOX??
+
 submitEl.addEventListener("click", function(event) {
-    var name = document.getElementById("textarea").textcontent;
-    document.getElementById("subtext").textContent = name;
-})
+    var name = tArea.value;
+    if (tArea !== null){
+        highScores.push(name + "-" + score);
+        storeHighS();
+    }
+    displayHighScores();//need to make display for highscores
+
+});
+
+clearEl.addEventListener("click", function(){
+    highScores = [];
+    storeHighS();
+    displayHighScores();
+});
 
 op1El.addEventListener("click", function() {
-    document.getElementById("foot").style.display = "block";
-    if(opList[promtNum][4] === 0) {
-        score++;
-        document.getElementById("foot").textContent = "Correct!";
-    } else {
-        document.getElementById("foot").textContent = "Wrong";
-    }
-    promtNum++;
-    if (promtNum === qList.length){
-        displayLast();//need to make display for last 2 sections (finish)
-    } else {
-        displayNext();
-    }
-    
+    opClick(0);
 });
 
 op2El.addEventListener("click", function() {
-    document.getElementById("foot").style.display = "block";
-    if(opList[promtNum][4] === 1) {
-        score++;
-        document.getElementById("foot").textContent = "Correct!";
-    } else {
-        document.getElementById("foot").textContent = "Wrong";
-    }
-    promtNum++;
-    if (promtNum === qList.length){
-        displayLast();//need to make display for last 2 sections (finish)
-    } else {
-        displayNext();
-    }
+    opClick(1);
     
 });
 
 op3El.addEventListener("click", function() {
-    document.getElementById("foot").style.display = "block";
-    if(opList[promtNum][4] === 2) {
-        score++;
-        document.getElementById("foot").textContent = "Correct!";
-    } else {
-        document.getElementById("foot").textContent = "Wrong";
-    }
-    promtNum++;
-    if (promtNum === qList.length){
-        displayLast();//need to make display for last 2 sections (finish)
-    } else {
-        displayNext();
-    }
-    
+    opClick(2);
 });
 
 op4El.addEventListener("click", function() {
-    document.getElementById("foot").style.display = "block";
-    if(opList[promtNum][4] === 3) {
-        score++;
-        document.getElementById("foot").textContent = "Correct!";
-    } else {
-        document.getElementById("foot").textContent = "Wrong";
-    }
-    promtNum++;
-    if (promtNum === qList.length){
-        displayLast();//need to make display for last 2 sections (finish)
-    } else {
-        displayNext();
-    }
-    
+    opClick(3);
 });
+
+init();
